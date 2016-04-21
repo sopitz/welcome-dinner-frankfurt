1a90f097817edb68f67434d919bd03d044e214bb
@_sopitz <simon.opitz@eventsos.de>
2016-04-21 16:43:51 +0200

    add docker and make it runable

--- sources/application/view/_templates/header.php
+++ sources/application/view/_templates/header.php
@@ -1,79 +1,47 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html class="nojs html" lang="de-DE">
 <head>
-    <title>HUGE</title>
-    <!-- META -->
-    <meta charset="utf-8">
-    <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
-    <link rel="icon" href="data:;base64,=">
+
+    <script type="text/javascript">
+        if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required":["jquery-1.8.3.min.js", "museutils.js", "jquery.musepolyfill.bgsize.js", "jquery.watch.js", "jquery.musemenu.js", "webpro.js", "jquery.scrolleffects.js", "index.css"], "outOfDate":[]};
+    </script>
+
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+    <meta name="generator" content="2015.1.2.344"/>
+    <title>Startseite</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/site_global.css?131700929"/>
+    <link rel="stylesheet" type="text/css" href="css/master_a-musters.css?444265083"/>
+    <link rel="stylesheet" type="text/css" href="css/index.css?3904742004" id="pagesheet"/>
+    <!-- Other scripts -->
+    <script type="text/javascript">
+        document.documentElement.className = document.documentElement.className.replace(/\bnojs\b/g, 'js');
+    </script>
+    <!-- JS includes -->
+    <!--[if lt IE 9]>
+    <script src="scripts/html5shiv.js?4241844378" type="text/javascript"></script>
+    <![endif]-->
+    <script type="text/javascript">
+        document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//use.typekit.net/ik/a04d-DZja31ZOSP23JUyeeay4IpGTE5Ky-9JAoIKhxIfe7jgfVKFa6IPH2whwDq3wD4LZ2F8w2icwRbaFR6ojDIa5QSk52BcFhbtjcwkw2IXwhj-Fai8deFzdAmq-Ao0ZWJlZ14ydclciW4XOcs8OfG0ihmCjhNlZAsTdco8SKucScB0-hZuS1b0Ze9KfhZTScIlShB0SkXkH6GJxCMgIMMjgPMgH6GJvwMgIMMjgKMgH6GJxvMgIMMj2kMgP6sFiWF8qMYAC-khgb.js" type="text/javascript">\x3C/script>');
+    </script>
+    <!-- Other scripts -->
+    <script type="text/javascript">
+        document.write('\x3Cscript src="' + (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//use.typekit.net/ik/a04d-DZja31ZOSP23JUyeeay4IpGTE5Ky-9JAoIKhxIfe7jgfVKFa6IPH2whwDq3wD4LZ2F8w2icwRbaFR6ojDIa5QSk52BcFhbtjcwkw2IXwhj-Fai8deFzdAmq-Ao0ZWJlZ14ydclciW4XOcs8OfG0ihmCjhNlZAsTdco8SKucScB0-hZuS1b0Ze9KfhZTScIlShB0SkXkH6GJxCMgIMMjgPMgH6GJvwMgIMMjgKMgH6GJxvMgIMMj2kMgP6sFiWF8qMYAC-khgb.js" type="text/javascript">\x3C/script>');
+    </script>
+    <!-- Other scripts -->
+    <script>
+        (function(d) {
+            var config = {
+                    kitId: 'izk3ekp',
+                    scriptTimeout: 3000,
+                    async: true
+                },
+                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+        })(document);
+    </script>
 </head>
 <body>
-    <!-- wrapper, to center website -->
-    <div class="wrapper">
 
-        <!-- logo -->
-        <div class="logo"></div>
+<div class="clearfix" id="page"><!-- column -->
 
-        <!-- navigation -->
-        <ul class="navigation">
-            <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
-            </li>
-            <li <?php if (View::checkForActiveController($filename, "profile")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-            </li>
-            <?php if (Session::userIsLoggedIn()) { ?>
-                <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
-                </li>
-                <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-                </li>
-            <?php } else { ?>
-                <!-- for not logged in users -->
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-                </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
-            <?php } ?>
-        </ul>
-
-        <!-- my account -->
-        <ul class="navigation right">
-        <?php if (Session::userIsLoggedIn()) : ?>
-            <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>user/index">My Account</a>
-                <ul class="navigation-submenu">
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/changeUserRole">Change account type</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/editAvatar">Edit your avatar</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/editusername">Edit my username</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/edituseremail">Edit my email</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "user")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>user/changePassword">Change Password</a>
-                    </li>
-                    <li <?php if (View::checkForActiveController($filename, "login")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>login/logout">Logout</a>
-                    </li>
-                </ul>
-            </li>
-            <?php if (Session::get("user_account_type") == 7) : ?>
-                <li <?php if (View::checkForActiveController($filename, "admin")) {
-                    echo ' class="active" ';
-                } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
-                </li>
-            <?php endif; ?>
-        <?php endif; ?>
-        </ul>
+</div>
--- sources/application/view/index/index.php
+++ sources/application/view/index/index.php
@@ -1,18 +1,192 @@
-<div class="container">
-    <h1>IndexController/index</h1>
-    <div class="box">
-
-        <!-- echo out the system feedback (error and success messages) -->
-        <?php $this->renderFeedbackMessages(); ?>
-
-        <h3>What happens here ?</h3>
-        <p>
-            This is the homepage. As no real URL-route (like /register/index) is provided, the app uses the default
-            controller and the default action, defined in application/config/config.php, by default it's
-            IndexController and index()-method. So, the app will load application/controller/IndexController.php and
-            run index() from that file. Easy. That index()-method (= the action) has just one line of code inside
-            ($this->view->render('index/index');) that loads application/view/index/index.php, which is basically
-            this text you are reading right now.
-        </p>
+<div class="position_content" id="page_position_content">
+    <div class="clearfix colelem" id="ppstartseite"><!-- group -->
+        <div class="clearfix grpelem" id="pstartseite"><!-- column -->
+            <a class="anchor_item colelem" id="startseite"></a>
+            <a class="anchor_item colelem" id="was_ist_welcome_dinner"></a>
+        </div>
+        <div class="browser_width grpelem" id="u333-bw">
+            <div class="museBGSize" id="u333"><!-- group -->
+                <div class="clearfix" id="u333_align_to_page">
+                    <a class="nonblock nontext anim_swing museBGSize ose_pre_init grpelem" id="u2848" href="index.html#was_ist_welcome_dinner"><!-- simple frame --></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix colelem" id="pu2453"><!-- group -->
+        <div class="browser_width mse_pre_init" id="u2453-bw">
+            <div class="shadow rgba-background" id="u2453"><!-- simple frame --></div>
+        </div>
+        <nav class="MenuBar clearfix mse_pre_init" id="menuu336"><!-- horizontal box -->
+            <div class="MenuItemContainer clearfix grpelem" id="u5026"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu MuseMenuActive clearfix colelem" id="u5029" href="index.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u5030-4"><!-- content --><p>Startseite</p></div></a>
+            </div>
+            <div class="MenuItemContainer clearfix grpelem" id="u365"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u366" href="welcome-dinner.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u369-4"><!-- content --><p>Welcome Dinner</p></div></a>
+            </div>
+            <div class="MenuItemContainer clearfix grpelem" id="u2675"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u2676" href="aktuelles.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u2679-4"><!-- content --><p>Aktuelles</p></div></a>
+            </div>
+            <div class="MenuItemContainer clearfix grpelem" id="u358"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u361" href="gastgeber-werden.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u363-4"><!-- content --><p>Gastgeber werden</p></div></a>
+            </div>
+            <div class="MenuItemContainer clearfix grpelem" id="u344"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u345" href="gast-werden.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u348-4"><!-- content --><p>Gast werden</p></div></a>
+            </div>
+            <div class="MenuItemContainer clearfix grpelem" id="u337"><!-- vertical box -->
+                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u338" href="partner.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u341-4"><!-- content --><p>Partner</p></div></a>
+            </div>
+        </nav>
+        <div class="clip_frame ose_pre_init mse_pre_init" id="u5497"><!-- image -->
+            <img class="block" id="u5497_img" src="images/logo_wdf_entwurf-2-08-07-07.png" alt="" width="57" height="57"/>
+        </div>
+        <div class="browser_width mse_pre_init" id="u334-3-bw">
+            <div class="rgba-background clearfix" id="u334-3"><!-- content -->
+                <p>&nbsp;</p>
+            </div>
+        </div>
+        <div class="clearfix mse_pre_init" id="u335-4"><!-- content -->
+            <p>Welcome Dinner Frankfurt</p>
+        </div>
+        <div class="clearfix mse_pre_init" id="u2568-4"><!-- content -->
+            <p>Für eine Willkommenskultur in unserer Stadt</p>
+        </div>
+        <div class="clip_frame mse_pre_init" id="u5291"><!-- image -->
+            <img class="block" id="u5291_img" src="images/logo_wdf_entwurf-2-06167x167.png" alt="" width="167" height="167"/>
+        </div>
+    </div>
+    <div class="clearfix colelem" id="u2440-4"><!-- content -->
+        <p>Was ist Welcome Dinner?</p>
+    </div>
+    <div class="clearfix colelem" id="u2384-17"><!-- content -->
+        <p>Frankfurt am Main – das ist die Stadt des Geldes, der regen Betriebsamkeit. Aber gleichwohl auch Stadt der Gemütlichkeit, Stadt der Gemeinschaft und Stadt der Offenheit. Frankfurt bietet auf engstem Raum eine soziale Vielfalt, die ihresgleichen sucht. Und ihre Bewohner schlagen seit jeher Brücken zwischen unterschiedlichsten Kulturen. Nun, wo eine Vielzahl von Menschen diese Stadt erreicht, in der sie sich – auf der Suche nach Schutz und Zuflucht – ein besseres, sicheres Leben erhoffen, gilt es erneut Brücken zu schlagen.</p>
+        <p>&nbsp;</p>
+        <p>Wie ginge das einfacher als anlässlich eines gemeinsamen <span id="u2384-5">Dinners?</span></p>
+        <p>&nbsp;</p>
+        <p>Ein gemeinsames Essen ist immer noch die beste Möglichkeit, um sich kennenzulernen.</p>
+        <p>Alteingesessene treffen auf Neuankömmlinge, Alt auf Jung. Gemeinsam muss dabei nicht einmal die Sprache sein – einzig das Interesse am Kulturaustausch sowie die Bereitschaft, einen Grundstein für ein segensreiches künftiges Miteinander zu legen, ist Voraussetzung.</p>
+        <p>&nbsp;</p>
+        <p>Gemeinsam lachen, gemeinsam genießen, Gemeinschaft leben, Trennung überwinden, Sprache lernen, Beziehung starten, Willkommen heißen, Integration leben - das wollen wir für und in Frankfurt!</p>
+        <p>&nbsp;</p>
+    </div>
+    <a class="nonblock nontext clip_frame colelem" id="u3729" href="welcome-dinner.html"><!-- image --><img class="block" id="u3729_img" src="images/weiterlesen-schwarz.png" alt="" width="40" height="41"/></a>
+    <div class="clearfix colelem" id="psei_gast"><!-- group -->
+        <a class="anchor_item grpelem" id="sei_gast"></a>
+        <div class="browser_width grpelem" id="u1012-bw">
+            <div class="museBGSize" id="u1012"><!-- group -->
+                <div class="clearfix" id="u1012_align_to_page">
+                    <a class="nonblock nontext anim_swing museBGSize ose_pre_init grpelem" id="u2825" href="index.html#sei_gast"><!-- simple frame --></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="browser_width colelem" id="u2637-bw">
+        <div id="u2637"><!-- column -->
+            <div class="clearfix" id="u2637_align_to_page">
+                <div class="clearfix colelem" id="u2444-4"><!-- content -->
+                    <p>sei Gast</p>
+                </div>
+                <div class="clearfix colelem" id="u2443-12"><!-- content -->
+                    <p id="u2443-2">Deine Flucht nahm ihr Ende in Frankfurt am Main, wo du Schutz und Sicherheit gefunden hast.</p>
+                    <p id="u2443-4">Nun möchtest du Anschluss finden in dieser neuen Stadt, innerhalb einer für dich fremden Kultur und die Gastfreundschaft der Menschen erfahren, welche hier leben und lieben?</p>
+                    <p id="u2443-5">&nbsp;</p>
+                    <p id="u2443-7">Du bist interessiert an einem unterhaltsamen Austausch und vielleicht sogar am Finden von ersten guten Freunden? Und all dies im Rahmen eines gemeinsamen Abendessens?</p>
+                    <p id="u2443-8">&nbsp;</p>
+                    <p id="u2443-10">Sei gespannt, was dich erwartet – weil Essen Brücken schlägt.</p>
+                </div>
+                <a class="nonblock nontext clip_frame colelem" id="u3709" href="gast-werden.html"><!-- image --><img class="block" id="u3709_img" src="images/weiterlesen-wei%c3%9f.png" alt="" width="40" height="41"/></a>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix colelem" id="pwerde_gastgeber"><!-- group -->
+        <a class="anchor_item grpelem" id="werde_gastgeber"></a>
+        <div class="browser_width grpelem" id="u2421-bw">
+            <div class="museBGSize" id="u2421"><!-- column -->
+                <div class="clearfix" id="u2421_align_to_page">
+                    <div class="position_content" id="u2421_position_content">
+                        <div class="colelem" id="u2867"><!-- simple frame --></div>
+                        <a class="nonblock nontext anim_swing museBGSize ose_pre_init colelem" id="u2888" href="index.html#werde_gastgeber"><!-- simple frame --></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="browser_width colelem" id="u2640-bw">
+        <div id="u2640"><!-- column -->
+            <div class="clearfix" id="u2640_align_to_page">
+                <div class="clearfix colelem" id="u2565-4"><!-- content -->
+                    <p>werde Gastgeber</p>
+                </div>
+                <div class="clearfix colelem" id="pu3739"><!-- group -->
+                    <a class="nonblock nontext clip_frame grpelem" id="u3739" href="gastgeber-werden.html"><!-- image --><img class="block" id="u3739_img" src="images/weiterlesen-schwarz.png" alt="" width="40" height="41"/></a>
+                    <div class="clearfix grpelem" id="u5791-16"><!-- content -->
+                        <p id="u5791-2">Für Gastgeber</p>
+                        <p id="u5791-3">&nbsp;</p>
+                        <p id="u5791-5">Du möchtest deiner Solidarität mit Flüchtlingen Ausdruck verleihen, indem du selbst Gastgeber eines „WelcomeDinner“ wirst? Ihnen einen Empfang voller Herzlichkeit bereiten und ihnen einen ersten Einblick in unsere Gemeinschaft und unser Zusammenleben geben - und dabei noch einen unterhaltsamen Abend genießen?</p>
+                        <p id="u5791-6">&nbsp;</p>
+                        <p id="u5791-8">Dann scheue dich nicht, Gastgeber zu werden und den Neuankömmlingen ein „WelcomeDinner“ zu bereiten. Alles, was du dafür tun musst, ist das nachfolgende Formular auszufüllen.</p>
+                        <p id="u5791-9">&nbsp;</p>
+                        <p id="u5791-10">&nbsp;</p>
+                        <p id="u5791-12">Weil Essen Brücken schlägt.</p>
+                        <p id="u5791-13">&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix colelem" id="pkontakt"><!-- group -->
+        <a class="anchor_item grpelem" id="kontakt"></a>
+        <div class="browser_width grpelem" id="u2548-bw">
+            <div class="museBGSize" id="u2548"><!-- group -->
+                <div class="clearfix" id="u2548_align_to_page">
+                    <a class="nonblock nontext anim_swing museBGSize ose_pre_init grpelem" id="u5406" href="index.html#kontakt"><!-- simple frame --></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix colelem" id="pu2989"><!-- group -->
+        <div class="browser_width grpelem" id="u2989-bw">
+            <div id="u2989"><!-- column -->
+                <div class="clearfix" id="u2989_align_to_page">
+                    <div class="clearfix colelem" id="u2983-4"><!-- content -->
+                        <p>Welcome Dinner auch in deiner Stadt!</p>
+                    </div>
+                    <div class="clearfix colelem" id="u4962-4"><!-- content -->
+                        <p>Möchtest Du aktiv werden und Welcome Dinner auch in Deiner Stadt ermöglichen? Dann schreib uns und wir helfen Dir beim Start.</p>
+                    </div>
+                    <a class="nonblock nontext clip_frame colelem" id="u4955" href="partner.html"><!-- image --><img class="block" id="u4955_img" src="images/weiterlesen-schwarz.png" alt="" width="40" height="41"/></a>
+                </div>
+            </div>
+        </div>
+        <form class="form-grp clearfix grpelem" id="widgetu2996" method="post" enctype="multipart/form-data" action="scripts/form-u2996.php"><!-- none box -->
+            <div class="fld-grp clearfix grpelem" id="widgetu3009" data-required="true"><!-- none box -->
+                <label class="fld-label actAsDiv clearfix grpelem" id="u3011-4" for="widgetu3009_input"><!-- content --><span class="actAsPara">Name:</span></label>
+                <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u3012-4"><!-- content --><input class="wrapped-input" type="text" spellcheck="false" id="widgetu3009_input" name="custom_U3009" tabindex="1"/><label class="wrapped-input fld-prompt" id="widgetu3009_prompt" for="widgetu3009_input"><span class="actAsPara">Namen eingeben</span></label></span>
+            </div>
+            <div class="fld-grp clearfix grpelem" id="widgetu3004" data-required="true" data-type="email"><!-- none box -->
+                <label class="fld-label actAsDiv clearfix grpelem" id="u3005-4" for="widgetu3004_input"><!-- content --><span class="actAsPara">E-Mail:</span></label>
+                <span class="fld-input NoWrap actAsDiv clearfix grpelem" id="u3006-4"><!-- content --><input class="wrapped-input" type="email" spellcheck="false" id="widgetu3004_input" name="Email" tabindex="2"/><label class="wrapped-input fld-prompt" id="widgetu3004_prompt" for="widgetu3004_input"><span class="actAsPara">E-Mail-Adresse eingeben</span></label></span>
+            </div>
+            <div class="clearfix grpelem" id="u3003-4"><!-- content -->
+                <p>Formular wird gesendet...</p>
+            </div>
+            <div class="clearfix grpelem" id="u2997-4"><!-- content -->
+                <p>Auf dem Server ist ein Fehler aufgetreten.</p>
+            </div>
+            <div class="clearfix grpelem" id="u3008-4"><!-- content -->
+                <p>Formular empfangen.</p>
+            </div>
+            <input class="submit-btn NoWrap grpelem" id="u2998-17" type="submit" value="" tabindex="4"/><!-- state-based BG images -->
+            <div class="fld-grp clearfix grpelem" id="widgetu2999" data-required="false"><!-- none box -->
+                <label class="fld-label actAsDiv clearfix grpelem" id="u3001-4" for="widgetu2999_input"><!-- content --><span class="actAsPara">Nachricht:</span></label>
+                <span class="fld-textarea actAsDiv clearfix grpelem" id="u3002-4"><!-- content --><textarea class="wrapped-input" id="widgetu2999_input" name="custom_U2999" tabindex="3"></textarea><label class="wrapped-input fld-prompt" id="widgetu2999_prompt" for="widgetu2999_input"><span class="actAsPara">Nachricht eingeben</span></label></span>
+            </div>
+        </form>
+        <a class="nonblock nontext anim_swing clip_frame grpelem" id="u5781" href="index.html#startseite" data-mu-ie-matrix="progid:DXImageTransform.Microsoft.Matrix(M11=-1,M12=0,M21=0,M22=-1,SizingMethod='auto expand')" data-mu-ie-matrix-dx="0" data-mu-ie-matrix-dy="0"><!-- image --><img class="block" id="u5781_img" src="images/pfeil-schwarz.png" alt="" width="96" height="96"/></a>
+    </div>
+    <div class="verticalspacer"></div>
+    <div class="clearfix colelem" id="pu6469-4"><!-- group -->
+        <a class="nonblock nontext clearfix grpelem" id="u6469-4" href="http://finallights-design.de"><!-- content --><p id="u6469-2">FinalLights-design © 2016</p></a>
+        <a class="nonblock nontext clearfix grpelem" id="u6483-4" href="impressum.html"><!-- content --><p>Impressum</p></a>
     </div>
 </div>
