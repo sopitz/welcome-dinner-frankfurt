4bbe0c5a4ed028415786c2f615e7ccc39a34f1eb
@_sopitz <simon.opitz@eventsos.de>
2016-05-06 15:07:03 +0200

    finalise guest page

--- sources/application/view/index/guest.php
+++ sources/application/view/index/guest.php
@@ -1,86 +1,165 @@
-<div class="clearfix" id="page"><!-- column -->
-    <div class="position_content" id="page_position_content">
-        <div class="browser_width colelem" id="u3766-bw">
-            <div class="museBGSize" id="u3766"><!-- group -->
-                <div class="clearfix" id="u3766_align_to_page">
-                    <div class="rgba-background clearfix grpelem" id="u3767"><!-- group -->
-                        <div class="clearfix grpelem" id="u3768-4"><!-- content -->
-                            <p>Gast werden</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="browser_width mse_pre_init" id="u3769-bw">
-            <div class="shadow rgba-background" id="u3769"><!-- simple frame --></div>
-        </div>
-        <div class="clip_frame ose_pre_init mse_pre_init" id="u5561"><!-- image -->
-            <img class="block" id="u5561_img" src="<?php echo Config::get('URL'); ?>public/images/logo_wdf_entwurf-2-08-07-07.png" alt="" width="71" height="71"/>
-        </div>
-        <nav class="MenuBar clearfix mse_pre_init" id="menuu3771"><!-- horizontal box -->
-            <div class="MenuItemContainer clearfix grpelem" id="u5047"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u5048" href="index.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u5049-4"><!-- content --><p>Startseite</p></div></a>
-            </div>
-            <div class="MenuItemContainer clearfix grpelem" id="u3779"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu MuseMenuActive clearfix colelem" id="u3780" href="welcome-dinner.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u3782-4"><!-- content --><p>Welcome Dinner</p></div></a>
-            </div>
-            <div class="MenuItemContainer clearfix grpelem" id="u3772"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u3773" href="aktuelles.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u3775-4"><!-- content --><p>Aktuelles</p></div></a>
-            </div>
-            <div class="MenuItemContainer clearfix grpelem" id="u3800"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u3801" href="gastgeber-werden.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u3802-4"><!-- content --><p>Gastgeber werden</p></div></a>
-            </div>
-            <div class="MenuItemContainer clearfix grpelem" id="u3793"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u3796" href="gast-werden.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u3799-4"><!-- content --><p>Gast werden</p></div></a>
-            </div>
-            <div class="MenuItemContainer clearfix grpelem" id="u3786"><!-- vertical box -->
-                <a class="nonblock nontext MenuItem MenuItemWithSubMenu transition clearfix colelem" id="u3787" href="partner.html"><!-- horizontal box --><div class="MenuItemLabel NoWrap transition clearfix grpelem" id="u3788-4"><!-- content --><p>Partner</p></div></a>
-            </div>
-        </nav>
-        <div class="clearfix colelem" id="u4278-19"><!-- content -->
-            <form action="<?php echo Config::get('URL'); ?>register/guest" method="post">
-                <input type="radio" name="gender" value="male"> male<br>
-                <input type="radio" name="gender" value="female"> female<br>
+<!DOCTYPE html>
+<html lang="de"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <style type="text/css">.pac-container{background-color:#fff;position:absolute!important;z-index:1000;border-radius:2px;border-top:1px solid #d9d9d9;font-family:Arial,sans-serif;box-shadow:0 2px 6px rgba(0,0,0,0.3);-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;overflow:hidden}.pac-logo:after{content:"";padding:1px 1px 1px 0;height:16px;text-align:right;display:block;background-image:url(https://maps.gstatic.com/mapfiles/api-3/images/powered-by-google-on-white3.png);background-position:right;background-repeat:no-repeat;background-size:120px 14px}.hdpi.pac-logo:after{background-image:url(https://maps.gstatic.com/mapfiles/api-3/images/powered-by-google-on-white3_hdpi.png)}.pac-item{cursor:default;padding:0 4px;text-overflow:ellipsis;overflow:hidden;white-space:nowrap;line-height:30px;text-align:left;border-top:1px solid #e6e6e6;font-size:11px;color:#999}.pac-item:hover{background-color:#fafafa}.pac-item-selected,.pac-item-selected:hover{background-color:#ebf2fe}.pac-matched{font-weight:700}.pac-item-query{font-size:13px;padding-right:3px;color:#000}.pac-icon{width:15px;height:20px;margin-right:7px;margin-top:6px;display:inline-block;vertical-align:top;background-image:url(https://maps.gstatic.com/mapfiles/api-3/images/autocomplete-icons.png);background-size:34px}.hdpi .pac-icon{background-image:url(https://maps.gstatic.com/mapfiles/api-3/images/autocomplete-icons_hdpi.png)}.pac-icon-search{background-position:-1px -1px}.pac-item-selected .pac-icon-search{background-position:-18px -1px}.pac-icon-marker{background-position:-1px -161px}.pac-item-selected .pac-icon-marker{background-position:-18px -161px}.pac-placeholder{color:gray}</style>
 
-                <input type="text" name="firstname" placeholder="firstname" required /><br>
-                <input type="text" name="lastname" placeholder="lastname" required /><br>
-                <input type="number" name="age" placeholder="age" required /><br>
-                <input type="tel" name="phone" placeholder="phone" required /><br>
-                <input type="email" name="mail" placeholder="mail" required /><br>
-                <input type="text" name="street" placeholder="street" required /><br>
-                <input type="text" name="zipCode" placeholder="zipCode" required /><br>
-                <input type="text" name="city" placeholder="city" required /><br>
+    <title>WelcomeDinner</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" media="all" href="<?php echo Config::get('URL'); ?>public/css/plain.css">
+</head>
+<body>
 
-
-
-
-                <input type="text" name="country" placeholder="country" required /><br>
-                <input type="checkbox" name="languages[]" value="german">german<br>
-                <input type="checkbox" name="languages[]" value="english">english<br>
-
-                <input type="checkbox" name="foodspecials[]" value="halal">Halal<br>
-                <input type="checkbox" name="foodspecials[]" value="nopork">kein Schweinefleisch<br>
-                <input type="checkbox" name="foodspecials[]" value="vegetarian">vegetarisch<br>
-                <textarea name="foodspecialsnotes" placeholder="sonstiges"></textarea><br />
-                <textarea name="bringalongs" placeholder="Freund? Kinder? Ehepartner? (Alter? Sprachen?)"></textarea><br />
-                <input type="text" name="welcomeDinnerOrigin" placeholder="welcomeDinnerOrigin" required /><br>
-                <textarea name="notes" placeholder="notes"></textarea><br />
-                <input type="submit" value="Submit">
-
-            </form>
-        </div>
-        <div class="museBGSize ose_pre_init mse_pre_init" id="u4596"><!-- simple frame --></div>
-        <div class="colelem" id="u4327"><!-- custom html -->
-
-        </div>
-        <div class="verticalspacer"></div>
-        <div class="clearfix colelem" id="pu6469-4"><!-- group -->
-            <a class="nonblock nontext clearfix grpelem" id="u6469-4" href="http://finallights-design.de"><!-- content --><p id="u6469-2">FinalLights-design © 2016</p></a>
-            <a class="nonblock nontext clearfix grpelem" id="u6483-4" href="impressum.html"><!-- content --><p>Impressum</p></a>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
         </div>
     </div>
 </div>
-<div class="preload_images">
-    <img class="preload" src="images/left-arrow.png" alt=""/>
-    <img class="preload" src="images/right-arrow.png" alt=""/>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="page-header">
+                <h1>
+                    Dieses Formular ist für Gäste.
+                    <small>
+                        <a href="http://gast.welcome-dinner.de/en/guests/new?cookie_test=1460374788">English version</a>
+                    </small>
+                </h1>
+                <h3>
+                    Gastgeber können sich <a href="http://anmeldung.welcome-dinner-frankfurt.de/host/register">hier</a> eintragen.
+                </h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-8">
+            <p>Du bist Flüchtling in Frankfurt am Main? Du möchtest dich gerne auf Deutsch, Englisch und auch mit Händen und Füßen austauschen?</p>
+            <p>Dann möchten dich Hamburger herzlich willkommen heißen mit einem Abendessen!</p>
+            <p>Wie das geht?</p>
+            <ol>
+                <li>Trag dich einfach in das Formular unten ein.</li>
+                <li>Wir rufen dich in den Wochen danach auf deinem Telefon an und besprechen mit dir, wer dich zu einem Welcome Dinner einlädt.</li>
+                <li>Hab ein tolles Abendessen! Du fährst (mit deiner Familie oder Freunden) zu deinem Gastgeber und ihr esst zusammen und lernt euch kennen. Dabei könnt ihr miteinander reden, Spiele spielen oder euch Fotos zeigen oder was ihr möchtet.</li>
+            </ol>
+            <p>Für Fragen und Kommentare komm melde Dich bitte bei uns: <a href="https://www.facebook.com/welcomedinnerfrankfurt">https://www.facebook.com/welcomedinnerfrankfurt</a></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <hr>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+
+            <form action="<?php echo Config::get('URL'); ?>register/guest" method="post">
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="gender">Geschlecht*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="radio" name="gender" value="male" required> männlich<br>
+                        <input type="radio" name="gender" value="female"> weiblich
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="gender">Vorname*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="firstname" placeholder="Vorname" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="lastname">Nachname*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="lastname" placeholder="Nachname" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="lastname">Alter*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="age" placeholder="Alter" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="phone">Telefon*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="tel" name="phone" placeholder="Telefon" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="mail">E-Mail*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="email" name="mail" placeholder="E-Mail" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="street">Straße & Hausnummer*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="street" placeholder="Straße & Hausnummer" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="zipCode">PLZ*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="zipCode" placeholder="PLZ" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="city">Stadt*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="city" placeholder="Stadt" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="city">Land*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="country" placeholder="Land" required />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="languages">Sprachkenntnisse*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="checkbox" name="languages[]" value="german">german<br>
+                        <input type="checkbox" name="languages[]" value="english">english<br>
+                        <input type="checkbox" name="languages[]" value="french">french<br>
+                        <input type="checkbox" name="languages[]" value="spanish">spanish<br>
+                        <input type="checkbox" name="languages[]" value="arabic">arabic<br>
+                        <textarea name="languagesnotes" placeholder="weitere Sprachkenntnisse"></textarea>
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="languages">Sprachkenntnisse*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="checkbox" name="foodspecials[]" value="halal">Halal<br>
+                        <input type="checkbox" name="foodspecials[]" value="nopork">kein Schweinefleisch<br>
+                        <input type="checkbox" name="foodspecials[]" value="vegetarian">vegetarisch<br>
+                        <textarea name="foodspecialsnotes" placeholder="sonstiges"></textarea><br />
+                    </div>
+                </div>
+
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="welcomeDinnerOrigin">Woher kennst Du Welcome Dinner?*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <input type="text" name="welcomeDinnerOrigin" placeholder="Woher kennst Du Welcome Dinner?" required />
+                    </div>
+                </div>
+                <br />
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="coHosts">Begleitung*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <textarea name="bringalongs" placeholder="Freund? Kinder? Ehepartner? (Alter? Sprachen?)"></textarea><br />
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="notes">Anmerkungen/wichtige Hinweise*</label>
+                    <div class="col-sm-12 col-md-10">
+                        <textarea name="notes" placeholder="Anmerkungen/wichtige Hinweise" required></textarea>
+                    </div>
+                </div>
+                <div class="form-group col-md-12">
+                    <label class="control-label col-sm-2" for="date"></label>
+                    <input type="submit" class="btn btn-primary" value="abschicken">
+                </div>
+            </form>
+        </div>
+    </div>
+
 </div>
